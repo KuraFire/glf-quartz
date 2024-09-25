d652865bc44474f6ce61a098dc55d40b89e5b749
Faruk Ates <farukates@me.com>
2024-09-24 19:41:44 -0700

    Update 404
    Make it better when there's nothing.

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,5 +1,6 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import { Nobr } from "../Nobr"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
@@ -11,13 +12,13 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
       <h1>{i18n(cfg.locale).pages.error.title}</h1>
       <p>
         If this page was linked to, it’s not (yet) finished or published. It might need some
-        financial support to be created.{" "}
-        <a href="https://venmo.com/farukspeaks">Venmo @FarukSpeaks</a> to support its creation, and
-        put the page name or essay title in your message.
+        financial support to be created. See the{" "}
+        <a href="/experiments/writing-experiment">Love First Writing Experiment</a> for more info.
       </p>
       <p>
-        The current threshold for essays to be put on top of the{" "}
-        <a href="/essays/to-write">to-write pile</a> is: <nobr>$55</nobr>.
+        The current minimum threshold for essays to be put on top of the{" "}
+        <a href="/essays/to-write">to-write pile</a> is: <Nobr>$55</Nobr>. Some pieces may require
+        more.
       </p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
